bcaf141c442972d6303b740f3a8f44f5f51dc1c4
“vktrkpv” <vktrkpb@icloud.com>
2025-03-17 13:20:01 -0300

    mobile version updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,46 @@
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import SearchBar from './SearchBar';
+import { useState } from 'react';
+import { FiMenu, FiX } from 'react-icons/fi';
 
 function Header() {
+  const [isMobileOpne, setIsMobileOpne] = useState(false);
+
   return (
-    <header className="fixed top-0 left-0 w-full bg-white shadow-md py-2 px-6 w-full z-50">
+    <header className="fixed top-0 left-0 w-full bg-white shadow-md py-2 px-6 z-50">
       <div className="container mx-auto flex justify-between items-center">
         <Link to="/">
           <img src={logo} alt="Valery Laser Studio" className="h-12" />
         </Link>
 
-        <nav className="flex">
+        {/* Buttons for mobile version */}
+
+        <div className="flex items-center space-x-3 md:hidden">
+          <Link to="/login" className="text-black hover:text-[#8cd0cb]">
+            Log In
+          </Link>
+          <Link to="/signup" className="text-black hover:text-[#8cd0cb]">
+            Sign Up
+          </Link>
+          <button className="bg-[#3A5D56] text-white px-3 py-1 rounded-lg text-sm hover:bg-[#8cd0cb]">
+            Book Appointmet
+          </button>
+        </div>
+
+        {/* Burger btn */}
+
+        <button
+        className="md:hidden text-black text-2xl focus:outline-none ml-4"
+        onClick={() => setIsMobileOpne(!isMobileOpne)}
+        >
+          {isMobileOpne ? <FiX/> : <FiMenu/>}
+        </button>
+
+{/* desktop version */}
+
+
+        <nav className="hidden md:flex">
           <ul className="flex space-x-8">
             <li>
               <Link to="/" className="text-black hover:text-[#8cd0cb]">
@@ -28,16 +58,16 @@ function Header() {
               </Link>
             </li>
             <li>
-            <Link to="/prices" className="text-black hover:text-[#8cd0cb]">
+              <Link to="/prices" className="text-black hover:text-[#8cd0cb]">
                 Prices
               </Link>
             </li>
             <li>
-            <Link to="/packages" className="text-black hover:text-[#8cd0cb]">
-            Bundles
+              <Link to="/packages" className="text-black hover:text-[#8cd0cb]">
+                Bundles
               </Link>
             </li>
-            
+
             <li>
               <Link to="/whyus" className="text-black hover:text-[#8cd0cb]">
                 Why Us?
@@ -52,11 +82,16 @@ function Header() {
         </nav>
 
         <div className="hidden md:block">
-          <SearchBar/>
+          <SearchBar />
         </div>
 
-        <div className="flex items-center space-x-4">
-          <Link to="/login" className="text-black hover:text-[#8cd0cb] cursor-pointer">
+{/* desktop btns  */}
+
+        <div className="hidden md:flex items-center space-x-4">
+          <Link
+            to="/login"
+            className="text-black hover:text-[#8cd0cb] cursor-pointer"
+          >
             Log In
           </Link>
 
@@ -71,6 +106,26 @@ function Header() {
           </button>
         </div>
       </div>
+
+      {/* dropdown menue */}
+
+      {isMobileOpne && (
+        <nav className="absolute top-[70px] right-4 w-64 bg-white/95 backdrop-blur-lg rounded-lg shadow-lg md:hidden transition-all duration-300 ease-in-out">
+          <ul className="flex flex-col space-y-2 py-4">
+          <li><Link to="/" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>Main</Link></li>
+            <li><Link to="/aboutus" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>About Us</Link></li>
+            <li><Link to="/services" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>Services</Link></li>
+            <li><Link to="/prices" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>Prices</Link></li>
+            <li><Link to="/packages" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>Bundles</Link></li>
+            <li><Link to="/whyus" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>Why Us?</Link></li>
+            <li><Link to="/contacts" className="block px-6 py-3 text-lg font-medium text-black hover:bg-[#8cd0cb]/20 rounded-lg transition-all" onClick={() => setIsMobileOpne(false)}>Contacts</Link></li>
+
+          </ul>
+
+        </nav>
+      )}
+
+
     </header>
   );
 }
--- src/components/auth/ResetPassword.tsx
+++ src/components/auth/ResetPassword.tsx
@@ -1,11 +1,58 @@
-function ResetPassword(){
-    return(
-        <div>
+import { useState } from "react";
 
-        </div>
-       
 
-    )
+function ResetPassword() {
+
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if(!email){
+      setMessage("Please enter a valid email address");
+      return;
+    }
+    console.log("EMail ", email);
+
+    setIsSubmited(true);
+
+    setMessage("If this email exists, you will receive a password reset link.");
+  }
+
+
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-gray-800 mb-4">Reset Password</h2>
+          <form className="space-y-4"
+          onSubmit={handleSubmit}
+          >
+            <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#A2DED0]"
+            />
+
+            <button 
+            className="w-full bg-[#A2DED0] text-white py-2 rounded-lg hover:bg-[#89CDBF] transition cursor-pointer"
+            >
+              Send Reset Link
+
+            </button>
+          </form>
+
+          {isSubmited && (
+    <p className="text-center text-gray-700 mt-4">{message}</p>
+)}
+      </div>
+    </div>
+  );
 }
 
 export default ResetPassword;
+
+
