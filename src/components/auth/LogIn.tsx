70a2254287b190bb9b1ae6d9b36cb72ec27cec3c
“vktrkpv” <vktrkpb@icloud.com>
2025-04-08 15:06:15 -0300

    fixed

--- src/components/auth/LogIn.tsx
+++ src/components/auth/LogIn.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
 
     if( email === mockAdmin.email && password === mockAdmin.password) {
       localStorage.setItem("isAdminLoggedIn", "true");
-      window.location.href = "/admin";
+      navigate('/admin');
       console.log("Logged in successfully!");
     }
     else {
